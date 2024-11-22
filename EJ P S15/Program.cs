1c54cf7cd2515102430833a4b89631c455ac66f7
UPN <UPN@ULCLAB405>
2024-11-22 09:47:34 -0500

    Agregar archivos de proyecto.

---
+++ EJ P S15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EJ_P_S15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Aleatorio = new Random();
+            Console.Write("Ingrese # de filas -> ");int F=int.Parse(Console.ReadLine());
+            Console.Write("Ingrese # de columnas -> ");int C=int.Parse(Console.ReadLine());
+            
+            //FILAS |
+            //COLUMNAS <-->
+
+            int[,] Matriz=new int[F,C]; // es lo mismo que int [][] Matriz = new int [][];
+            for (int i = 0; i < Matriz.GetLength(0); i++)//el "GetLength(0)" para buscar en las filas
+            {
+                for (int j = 0; j < Matriz.GetLength(1); j++)//El "GetLenght(1)" para buscar en las columnas
+                {
+                    Matriz[i, j] = Aleatorio.Next(30, 51);
+                    Console.Write(Matriz[i, j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EJ4 P S15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace EJ4_P_S15
+{
+    internal class Program
+    {
+        public static void Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bienvenidos al sistema de registro de productos:\n");
+            Console.WriteLine("1.- Registrar Productos\n2.- Mostrar Productos\n3.- Modificar Producto\n0.- SALIR\n");
+            Console.Write("Seleccione una opción -> ");
+        }
+        static void Main(string[] args)
+        {
+            byte OCP;
+            Menu();
+            while (!byte.TryParse(Console.ReadKey(true).KeyChar.ToString(), out OCP)) { }
+            Console.ReadKey();
+        }
+    }
+}
